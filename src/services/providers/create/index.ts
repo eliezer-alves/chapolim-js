8049a11009ce95a3cbc9cf262c6a9521534d9648
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2023-07-19 00:48:19 -0300

    feat: (providers create) option to create the index file

--- src/services/providers/create/index.ts
+++ src/services/providers/create/index.ts
@@ -1,9 +1,14 @@
 const fs = require('fs')
 
 const PROVIDER_STUB = fs.readFileSync(`${__dirname}/stubs/provider.stub`, 'utf8')
+const PROVIDER_INDEX_STUB = fs.readFileSync(`${__dirname}/stubs/provider-index.stub`, 'utf8')
 
 function geStub(name: string) {
   return PROVIDER_STUB.replaceAll('$name', name)
+}
+
+function geIndexStub(name: string) {
+  return PROVIDER_INDEX_STUB.replaceAll('$name', name)
 }
 
 function createProviderFolder(folderPath: string) {
@@ -15,7 +20,18 @@ const createFile = (filePath: string, stub: string) => {
   console.log('Provider file: ' + filePath)
 }
 
-export function create(appBasePath: string, moduleName: string, name: string) {
+const createIndexProvider = (modulePath: string, name: string) => {
+  const stub = geIndexStub(name)
+  const filePath = `${modulePath}/providers/index.tsx`
+  if (fs.existsSync(filePath)) {
+    console.log(`${filePath}: already exists`)
+    return
+  }
+
+  return createFile(filePath, stub)
+}
+
+export function create(appBasePath: string, moduleName: string, name: string, createIndex = false) {
   moduleName = `${moduleName}Module`.replace('ModuleModule', 'Module')
   const mutatedName = `${name}Provider`.replace('ProviderProvider', 'Provider')
   const modulePath = `${appBasePath}/src/modules/${moduleName}`
@@ -29,8 +45,11 @@ export function create(appBasePath: string, moduleName: string, name: string) {
   }
 
   createProviderFolder(fileFolderPath)
-
   createFile(filePath, stub)
+
+  if (createIndex) {
+    createIndexProvider(modulePath, mutatedName)
+  }
 
   return 0
 }
